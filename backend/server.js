3b590a74808ba19d52b7dce928114121e5579820
AntheaMeier <72684855+AntheaMeier@users.noreply.github.com>
2022-09-20 10:15:46 +0200

    initial server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const postsRoutes = require('./routes/posts.routes');
+const subscriptionRoute = require('./routes/subscription.routes');
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/posts', postsRoutes);
+app.use('/subscription', subscriptionRoute);
+
+app.listen(process.env.PORT, (error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`server running on http://localhost:${process.env.PORT}`);
+    }
+});
+
+// connect to mongoDB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('connected to DB');
+});
