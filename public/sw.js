9a6aad93cfb46ebbac5dd442e79e2839678851c2
AntheaMeier <72684855+AntheaMeier@users.noreply.github.com>
2022-09-21 14:59:20 +0200

    CSS Anpassungen padding um Headings etc.

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 importScripts('/src/js/idb.js');
 importScripts('/src/js/db.js');
 
-const CACHE_VERSION = 76;
+const CACHE_VERSION = 100;
 const CURRENT_STATIC_CACHE = 'static-v'+CACHE_VERSION;
 const CURRENT_DYNAMIC_CACHE = 'dynamic-v'+CACHE_VERSION;
 
